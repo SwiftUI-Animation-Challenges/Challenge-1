9e21576a2c41375b3b9feb4a38d6a8442d3d0af3
adamsocki <socki.adam@gmail.com>
2022-07-14 19:05:49 +0200

    InitialCommit

---
+++ AnimationChallenge/ChartView.swift
@@ -0,0 +1,20 @@
+//
+//  ChartView.swift
+//  AnimationChallenge
+//
+//  Created by Adam Socki on 7/14/22.
+//
+
+import SwiftUI
+
+struct ChartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ChartView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChartView()
+    }
+}
