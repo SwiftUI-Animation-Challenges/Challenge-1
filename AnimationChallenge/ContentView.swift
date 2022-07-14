f56c0ef94e5404ef4102ef2de5b759a2ce50acf7
adamsocki <socki.adam@gmail.com>
2022-07-14 19:03:48 +0200

    Initial Commit

---
+++ AnimationChallenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AnimationChallenge
+//
+//  Created by Adam Socki on 7/14/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
