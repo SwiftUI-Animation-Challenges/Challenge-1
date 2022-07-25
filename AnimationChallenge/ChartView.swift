dfe75d25b6a4aa2eeccda669317c1da799e325a9
AdamS <65431559+adamsocki@users.noreply.github.com>
2022-07-25 21:05:06 +0300

    Update ChartView.swift

--- AnimationChallenge/ChartView.swift
+++ AnimationChallenge/ChartView.swift
@@ -14,7 +14,6 @@ struct ChartView: View {
     
     @State private var heightsIndex = 0
     let timer = Timer.publish(every: 0.2, on: .main, in: .common).autoconnect()
-    //let timer2 = Timer.publish(every: 0.5, on: .main, in: .common).autoconnect()
     @State private var heights: [CGFloat] = [20, 40, 60, 80, 100]
     var body: some View {
         ZStack {
@@ -27,34 +26,21 @@ struct ChartView: View {
                     .frame(width: 25, height: heights[0], alignment: .leading)
                     .offset(x: 0, y: -( heights[0]/2) )
                     .animation(.easeInOut, value: heights[0])
-//                    .onReceive(timer) { _ in
-//                        height = CGFloat(arc4random_uniform(100))
-//                    }
                 Rectangle()
                     .foregroundColor(.green)
                     .frame(width: 25, height: heights[1], alignment: .leading)
                     .offset(x: 0, y: -( heights[1]/2) )
                     .animation(.easeInOut, value: heights[1])
-//                    .onReceive(timer2) { _ in
-//                        height = CGFloat(arc4random_uniform(2 ))
-//                    }
                 Rectangle()
                     .foregroundColor(.green)
                     .frame(width: 25, height: heights[2], alignment: .leading)
                     .offset(x: 0, y: -( heights[2]/2) )
                     .animation(.easeInOut, value: heights[2])
-////                    .onReceive(timer) { _ in
-////                        height = 5
-////                    }
                 Rectangle()
                     .foregroundColor(.green)
                     .frame(width: 25, height: heights[3], alignment: .leading)
                     .offset(x: 0, y: -( heights[3]/2) )
                     .animation(.easeInOut, value: heights[3])
-////                    .onReceive(timer) { _ in
-////                        height = CGFloat.random(in: 1...100)
-////                    }
-                    
             }
         }
         .onReceive(timer) { _ in
@@ -65,7 +51,6 @@ struct ChartView: View {
             }
         }
         .onAppear {
-           
         }
         
     }
