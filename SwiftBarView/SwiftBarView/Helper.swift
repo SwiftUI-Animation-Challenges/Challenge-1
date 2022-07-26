2e58c13db5aeb9c99ee8915b22dd5801ae13e0dd
jijotp <63841664+jijotp@users.noreply.github.com>
2022-07-26 21:46:29 +0530

    animation Added

---
+++ SwiftBarView/SwiftBarView/Helper.swift
@@ -0,0 +1,15 @@
+//
+//  Helper.swift
+//  SwiftBarView
+//
+//  Created by QBuser on 26/07/22.
+//
+
+import Foundation
+import UIKit
+
+extension UIScreen {
+   static let screenWidth = UIScreen.main.bounds.size.width
+   static let screenHeight = UIScreen.main.bounds.size.height
+   static let screenSize = UIScreen.main.bounds.size
+}
