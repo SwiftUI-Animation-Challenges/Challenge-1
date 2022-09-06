14597fefd9b3ff5d5b4847aceea2e6644696eeaf
Jef-in <jefinabduljaleel@gmail.com>
2022-09-06 19:01:45 +0530

    initial project created

---
+++ Jefin Abdul Jaleel/Spotify Animation/Spotify Animation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Spotify Animation
+//
+//  Created by Jefin on 06/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Jefin Abdul Jaleel/Spotify Animation/Spotify Animation/Spotify_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spotify_AnimationApp.swift
+//  Spotify Animation
+//
+//  Created by Jefin on 06/09/22.
+//
+
+import SwiftUI
+
+@main
+struct Spotify_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
